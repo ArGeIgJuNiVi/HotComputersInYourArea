b4f63c7da53108ad693f37b0fe5a1d924e2969f0
Nicon-Georgian Albu <nicon@containrr.com>
2024-09-27 13:11:39 +0200

    crucial update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/the/meaning/of/life/the/universe/and/everythin{_}", function () {
+    return "42";
 });
